c8296a70b7ee4e15a33abac8d6ae661de01b6b0e
Sean Seymour <6946453+seanjseymour@users.noreply.github.com>
2022-03-19 14:34:39 -0700

    Start of tempating

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import * as Vue from "vue/dist/vue.esm-bundler.js";
 const Num = {
   props: ["number"],
   template: `
-    <div
+    <button
     v-bind:class="getClass(number)"
+    v-on:click="click"
     >
     {{ number }}
-    </div>
+    </button>
   `,
   methods: {
+    click(number) {
+      this.$emit('chosen', this.number)
+    },
     getClass(number) {
       return this.isEven(number) ? "blue" : "red";
     },
@@ -26,40 +30,26 @@ const app = Vue.createApp({
   },
 
   template: `
-    <button v-on:click="increment">Increment</button>
-    <p>{{ count }}</p>
-
-    <hello greeting="Hello!"/>
-
-    <input
-      type="checkbox"
-      v-model="value"
-      value="a"
-    />
-    <input
-      type="checkbox"
-      v-model="value"
-      value="b"
-    />
-
-    {{ value }}
-
-    <div class="red">
-      {{ error }}
-    </div>
-
     <num
       v-for="number in numbers"
       v-bind:number="number"
+      v-on:chosen="addNumber"
     />
-  `,
+
+    <hr />
+    
+    <num
+      v-for="number in numberHistory"
+      v-bind:number="number"
+    />
+
+    `,
 
   // Anything in data is reactive (automatically updates the DOM).
   data() {
     return {
-      count: 0,
-      value: ["a"],
       numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      numberHistory: [],
     };
   },
 
@@ -69,21 +59,12 @@ const app = Vue.createApp({
     evenList() {
       return this.numbers.filter((num) => this.isEven(num));
     },
-    error() {
-      if (this.value.length < 4) {
-        return "Username must be greater than 4.";
-      }
-    },
   },
 
   // Methods can access anything returned in the data section using "this"
   methods: {
-    // Example of two way binding, was replaced (along with v-bind & v-on in template) by v-model
-    /*input($event) {
-      this.value = $event.target.value;
-    },*/
-    increment() {
-      this.count += 1;
+    addNumber(number) {
+      this.numberHistory.push(number);
     },
   },
 });
